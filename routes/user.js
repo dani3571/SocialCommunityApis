584b7125176598fd18d9252def0b0361292d221b
Daniel <ccd2024081@est.univalle.edu>
2024-04-30 18:45:03 -0400

    Added prisma.io

--- routes/user.js
+++ routes/user.js
@@ -1,28 +1,99 @@
-const express = require("express");
-const router = express.Router(); // Utiliza Router en lugar de crear una nueva instancia de Express
+const express = require('express');
+const { PrismaClient } = require('@prisma/client');
 
-const dotenv = require("dotenv");
-dotenv.config();
+const prisma = new PrismaClient()
+const router = express.Router()
 
-//conexión con la base de datos
-const { connection } = require("../config.db");
+router.get('/user', async(req, res) => {
+    try {
+        const users = await prisma.user.findMany();
+        res.json(users);
+    } catch (error) {
+        console.error('Error al obtener usuarios:', error);
+        res.status(500).send('Error interno del servidor');
+    }
+})
 
-const getUser = (request, response) => {
-    connection.query("SELECT * FROM user", (error, results) => {
-        if (error)
-            throw error;
-        response.status(200).json(results);
-    });
-};
+router.post('/user/create', async(req, res) => {
+    try {
+        const { name_user, email_user, password_user, active_user, created_user, id_rol } = req.body
+        const user_create = await prisma.user.create({
+            data: {
+                name_user,
+                email_user,
+                password_user,
+                active_user : true,
+                created_user: new Date(), 
+                rol: { // Aquí proporcionas la información del rol
+                    connect: { id_rol: 1 } // Aquí especificas el rol al que pertenece el usuario (por ejemplo, el rol con id_rol = 1)
+                },
+                profile: { // Aquí proporcionas la información del rol
+                    connect: { id_profile: 1 } // Aquí especificas el rol al que pertenece el usuario (por ejemplo, el rol con id_rol = 1)
+                }
+              },
+        });
+        res.json(user_create);
+    } catch (error) {
+        console.error('Error al crear usuario:', error);
+        res.status(500).send('Error interno del servidor');
+    }
+})
 
+router.put('/user/update/:id', async(req, res) => {
+    const userId = parseInt(req.params.id); // Obtener el ID del usuario de los parámetros de la URL
+    const { name_user, email_user, password_user, active_user, created_user, id_rol } = req.body;
 
-// Definir la ruta en el enrutador
-router.get("/", (req, res) => {
-    res.send("Bienvenido a la página principal");
+    try {
+        const existingUser = await prisma.user.findUnique({
+            where: { id_user: userId }
+        });
+
+        if (!existingUser) {
+            return res.status(404).json({ message: 'Usuario no encontrado' });
+        }
+
+        const updatedUser = await prisma.user.update({
+            where: { id_user: userId },
+            data: {
+                name_user,
+                password_user,
+            }
+        });
+
+        res.json(updatedUser);
+    } catch (error) {
+        console.error('Error al actualizar usuario:', error);
+        res.status(500).send('Error interno del servidor');
+    }
 });
 
-router.get("/user", getUser);
+router.put('/user/inactive/:id', async(req, res) => {
+    const userId = parseInt(req.params.id);
+    try {
+        const existingUser = await prisma.user.findUnique({
+            where: { id_user: userId }
+        });
+
+        if (!existingUser) {
+            return res.status(404).json({ message: 'Usuario no encontrado' });
+        }
+
+        const updatedUser = await prisma.user.update({
+            where: { id_user: userId },
+            data: {
+                active_user: false
+            }
+        });
+
+        res.json(updatedUser);
+    } catch (error) {
+        console.error('Error al actualizar usuario:', error);
+        res.status(500).send('Error interno del servidor');
+    }
+});
 
 
-// Exportar el enrutador para que pueda ser utilizado por la aplicación principal
+
+
+// Exporta el enrutador
 module.exports = router;
