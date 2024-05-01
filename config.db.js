1d2d14ad21a0d719ebe36d527210228b2bb008ea
Daniel Chavez <ccd2024081@est.univalle.edu>
2024-05-01 13:25:16 -0400

    Added jwt, login and logout

--- config.db.js
+++ config.db.js
@@ -10,9 +10,10 @@ try {
     connection = mysql.createConnection({
         host: process.env.DBHOST,
         user: process.env.DBUSER,
-   //     password: process.env.DBPASS,
+        password: process.env.DBPASS,
         database: process.env.DBNAME 
     });
+
 } catch (error) {
     console.log("Error al conectar con la base de datos");
 }
--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const { PrismaClient } = require('@prisma/client');
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
 
 const prisma = new PrismaClient()
 const router = express.Router()
@@ -17,19 +20,17 @@ router.get('/user', async(req, res) => {
 router.post('/user/create', async(req, res) => {
     try {
         const { name_user, email_user, password_user, active_user, created_user, id_rol } = req.body
+        const hashedPassword = await bcrypt.hash(password_user,10)
         const user_create = await prisma.user.create({
             data: {
                 name_user,
                 email_user,
-                password_user,
+                password_user: hashedPassword,
                 active_user : true,
                 created_user: new Date(), 
-                rol: { // Aquí proporcionas la información del rol
-                    connect: { id_rol: 1 } // Aquí especificas el rol al que pertenece el usuario (por ejemplo, el rol con id_rol = 1)
+                rol: { 
+                    connect: { id_rol: 1 } 
                 },
-                profile: { // Aquí proporcionas la información del rol
-                    connect: { id_profile: 1 } // Aquí especificas el rol al que pertenece el usuario (por ejemplo, el rol con id_rol = 1)
-                }
               },
         });
         res.json(user_create);
@@ -40,7 +41,7 @@ router.post('/user/create', async(req, res) => {
 })
 
 router.put('/user/update/:id', async(req, res) => {
-    const userId = parseInt(req.params.id); // Obtener el ID del usuario de los parámetros de la URL
+    const userId = parseInt(req.params.id);
     const { name_user, email_user, password_user, active_user, created_user, id_rol } = req.body;
 
     try {
@@ -51,15 +52,21 @@ router.put('/user/update/:id', async(req, res) => {
         if (!existingUser) {
             return res.status(404).json({ message: 'Usuario no encontrado' });
         }
+        const dataToUpdate = {
+            name_user,
+        };
+
+        if (password_user) {
+            dataToUpdate.password_user =  await bcrypt.hash(password_user, 10);
+        }
 
         const updatedUser = await prisma.user.update({
             where: { id_user: userId },
-            data: {
-                name_user,
-                password_user,
-            }
+            data: dataToUpdate
         });
 
+
+      
         res.json(updatedUser);
     } catch (error) {
         console.error('Error al actualizar usuario:', error);
@@ -91,9 +98,61 @@ router.put('/user/inactive/:id', async(req, res) => {
         res.status(500).send('Error interno del servidor');
     }
 });
+router.put('/user/activate_user/:id', async(req, res) => {
+    const userId = parseInt(req.params.id);
+    try {
+        const existingUser = await prisma.user.findUnique({
+            where: { id_user: userId }
+        });
+
+        if (!existingUser) {
+            return res.status(404).json({ message: 'Usuario no encontrado' });
+        }
+
+        const updatedUser = await prisma.user.update({
+            where: { id_user: userId },
+            data: {
+                active_user: true
+            }
+        });
+
+        res.json(updatedUser);
+    } catch (error) {
+        console.error('Error al actualizar usuario:', error);
+        res.status(500).send('Error interno del servidor');
+    }
+});
 
 
+router.post('/login', async (req, res) => {
+    const { email_user, password_user } = req.body;
 
+    try {
+        const user = await prisma.user.findFirst({
+            where: { email_user }
+        });
+
+        if (!user || !(await bcrypt.compare(password_user, user.password_user))) {
+            return res.status(401).json({ message: 'Credenciales inválidas' });
+        }
+
+        // Genera JWT
+        const token = jwt.sign({ userId: user.id_user }, 'secreto', { expiresIn: '1h' });
+
+        res.json({ token });
+    } catch (error) {
+        console.error('Error al iniciar sesión:', error);
+        res.status(500).send('Error interno del servidor');
+    }
+});
+
+
+router.post('/logout', async (req, res) => {
+   
+    res.clearCookie('token').json({ message: 'Logout exitoso' });
+});
+
+module.exports = router;
 
 // Exporta el enrutador
 module.exports = router;
